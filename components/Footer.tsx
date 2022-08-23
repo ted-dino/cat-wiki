26cdb6022c005e5edb510cd0190d1b71aaf52c58
Ted Diño <ted23@tutanota.com>
2022-08-23 12:16:40 +0800

    Refactor: display current year and add scroll to top

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,20 +2,23 @@ import Image from "next/image";
 import Link from "next/link";
 
 const Footer = () => {
+  const currYear = new Date().getFullYear();
   return (
     <footer className="p-4 md:p-9 bg-black rounded-t-[40px] flex flex-wrap items-center justify-center md:justify-between">
-      <Image
-        src="/CatwikiLogo.svg"
-        width={123}
-        height={43}
-        className="brightness-0 invert"
-      />
+      <Link href="#top">
+        <Image
+          src="/CatwikiLogo.svg"
+          width={123}
+          height={43}
+          className="brightness-0 invert"
+        />
+      </Link>
       <span className="text-white text-center">
         created by{" "}
         <Link href="https://github.com/ted-dino">
           <a className="font-bold">Ted Dino</a>
         </Link>{" "}
-        - devChallenge.io 2021
+        - devChallenge.io {currYear}
       </span>
     </footer>
   );
