4b81854a569707dd0a6daff9db432b51e8496e09
Ted Diño <ted23@tutanota.com>
2022-08-23 14:32:41 +0800

    Refactor: add api for getting all cats

---
+++ pages/api/cats.ts
@@ -0,0 +1,25 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { Cat } from "../../typings";
+import { config } from "../../utils/config";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const result = await fetch(
+      "https://api.thecatapi.com/v1/breeds?attach_breed=0",
+      {
+        headers: { "X-API-KEY": config.apiKey },
+      }
+    );
+    const data: Cat = await result.json();
+    res.status(200).json(data);
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error(error.message);
+    }
+    throw new Error(`Unexpected Error: ${error}`);
+  }
+}
