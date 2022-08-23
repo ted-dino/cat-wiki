e7fc6c7666e4b152741d8fc0409ed0690fa0a87f
Ted Diño <ted23@tutanota.com>
2022-08-23 09:42:47 +0800

    Refactor: add back to home link

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 const Header = () => {
   return (
     <header className="pt-5">
-      <Image src="/CatwikiLogo.svg" width={123} height={43} />
+      <Link href="/">
+        <a>
+          <Image src="/CatwikiLogo.svg" width={123} height={43} />
+        </a>
+      </Link>
     </header>
   );
 };
