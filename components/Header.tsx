a49cab51381a3b85bb112ceec1d6d1416143c338
Ted Diño <ted23@tutanota.com>
2022-08-23 12:27:39 +0800

    Refactor: add id

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 const Header = () => {
   return (
-    <header className="pt-5">
+    <header id="top" className="pt-5">
       <Link href="/">
         <a>
           <Image src="/CatwikiLogo.svg" width={123} height={43} />
